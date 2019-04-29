9deba6570ce2a868fc721a362e19a26e091d9e14
Matheus Zanin <matt_z6@hotmail.com>
2019-04-29 08:41:06 -0300

    Algumas alterações

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="page-footer font-small special-color-dark pt-4">
+  <!-- <footer class="page-footer font-small pt-4 bgEscuro">
     <div class="container">
       <div class="row">
         <div class="col-md-6 mb-4">
@@ -10,7 +10,7 @@
               placeholder="Search"
               aria-label="Search"
             >
-            <i class="fas fa-search" aria-hidden="true"></i>
+            <i class="fa fa-search" aria-hidden="true"></i>
           </form>
         </div>
 
@@ -34,6 +34,64 @@
       © 2018 Copyright:
       <a href="https://mdbootstrap.com/education/bootstrap/">MDBootstrap.com</a>
     </div>
+  </footer>-->
+
+  <footer class="page-footer font-small blue pt-4 mt-5">
+    <div class="container">
+      <div class="container-fluid text-center text-md-left">
+        <div class="row">
+          <div class="col-md-6 mt-md-0 mt-3">
+            <h5>Obrigado por acessar nossa Lojinha!</h5>
+            <p>Você será sembre bem-vindo aqui! 😊</p>
+          </div>
+
+          <hr class="clearfix w-100 d-md-none pb-3">
+
+          <div class="col-md-3 mb-md-0 mb-3">
+            <h5 class="text-uppercase">Nossas categorias</h5>
+
+            <ul class="list-unstyled">
+              <li>
+                <a href="#!">Link 1</a>
+              </li>
+              <li>
+                <a href="#!">Link 2</a>
+              </li>
+              <li>
+                <a href="#!">Link 3</a>
+              </li>
+              <li>
+                <a href="#!">Link 4</a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="col-md-3 mb-md-0 mb-3">
+            <h5 class="text-uppercase">Links</h5>
+
+            <ul class="list-unstyled">
+              <li>
+                <a href="#!">Link 1</a>
+              </li>
+              <li>
+                <a href="#!">Link 2</a>
+              </li>
+              <li>
+                <a href="#!">Link 3</a>
+              </li>
+              <li>
+                <a href="#!">Link 4</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="footer-copyright text-center py-3">
+        © 2018 Copyright:
+        <a href="https://mdbootstrap.com/education/bootstrap/">MDBootstrap.com</a>
+      </div>
+    </div>
   </footer>
 </template>
 
@@ -42,3 +100,7 @@ export default {
   name: "Footer"
 };
 </script>
+
+<style lang='scss' scoped>
+@import "./styles.scss";
+</style>
--- src/components/NavBar/index.vue
+++ src/components/NavBar/index.vue
@@ -1,34 +1,54 @@
 <template>
-  <nav class="navbar sticky-top navbar-expand-lg navbar-dark pt-2 pb-2 corNav">
-    <div class="container d-flex justify-content-between">
-      <router-link class="navbar-brand" to="/">
-        <img
-          src="@/assets/icon.png"
-          width="30"
-          height="30"
-          class="d-inline-block align-top"
-          alt="Icone"
-        >
-        Lojinha
-      </router-link>
+  <div>
+    <nav class="navbar sticky-top navbar-expand-lg navbar-dark pt-2 pb-2 bgEscuro">
+      <div class="container d-flex justify-content-between">
+        <router-link class="navbar-brand" to="/">
+          <img
+            src="@/assets/icon.png"
+            width="30"
+            height="30"
+            class="d-inline-block align-top"
+            alt="Icone"
+          >
+          Lojinha
+        </router-link>
 
-      <form class="col-7 input-group">
-        <input
-          class="form-control form-control-sm"
-          type="search"
-          placeholder="O que você procura? 💬"
-          aria-label="O que você procura?"
-        >
-        <div class="input-group-append">
-          <button class="btn btn-sm btn-light" type="button">
-            <i class="fa fa-search"></i>
-          </button>
-        </div>
-      </form>
+        <form class="col-7 input-group">
+          <input
+            class="form-control form-control-sm"
+            type="search"
+            placeholder="O que você procura? 💬"
+            aria-label="O que você procura?"
+          >
+          <div class="input-group-append">
+            <button class="btn btn-sm btn-light" type="button">
+              <i class="fa fa-search"></i>
+            </button>
+          </div>
+        </form>
 
-      <button type="button" class="btn btn-outline-light btn-sm">Olá</button>
-    </div>
-  </nav>
+        <button type="button" class="btn btn-md corClara">
+          <span class="badge badge-pill badge-danger">4</span>
+          <i class="fa fa-shopping-cart"></i>
+        </button>
+      </div>
+    </nav>
+
+    <ul class="nav pt-2 justify-content-center">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -37,9 +57,6 @@ export default {
 };
 </script>
 
-
 <style lang='scss' scoped>
 @import "./styles.scss";
 </style>
-
-
